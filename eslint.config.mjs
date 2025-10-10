e7e0be691a28bc4b1bd22d244ade97577c31a2b6
jaycodev <jason.vilac@gmail.com>
2025-10-10 09:16:53 -0500

    ♻️ refactor(paths): remove folder aliases and use only @/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -55,7 +55,7 @@ export default defineConfig([
           groups: [
             ['^react$', '^react/'],
             ['^@?\\w'],
-            ['^@components', '^@lib', '^@hooks'],
+            ['^@/components', '^@/lib', '^@/hooks'],
             ['^\\.\\.(?!/?$)', '^\\./'],
             ['\\.css$', '\\.scss$', '\\.less$'],
           ],
